cf7661e50ca39ce6c5d799a82ae5cd3fcaee155d
Arik Maor <arikmaor@gmail.com>
2020-06-18 00:50:32 +0300

    Refactor mongorc

--- .mongorc.js
+++ .mongorc.js
@@ -1,26 +1,27 @@
 let lastProfilingLevel;
 let profilingStartTs;
 
-function startProfiling() {
-  lastProfilingLevel = db.getProfilingLevel();
-  if (lastProfilingLevel !== 2) {
-    print(`Setting profiling level to 2 (was ${lastProfilingLevel}).`);
-    db.setProfilingLevel(2);
-  }
-  profilingStartTs = ISODate();
-}
-
-function stopProfiling() {
-  if (lastProfilingLevel !== 2) {
-    print(`Setting profiling level back to ${lastProfilingLevel}.`);
-    db.setProfilingLevel(2);
-  }
-  db.system.profile.find({
-    op: 'query',
-    ts: {
-      $gt: profilingStartTs,
-      $lte: ISODate()
+profiling = {
+  start() {
+    lastProfilingLevel = db.getProfilingLevel();
+    if (lastProfilingLevel !== 2) {
+      print(`Setting profiling level to 2 (was ${lastProfilingLevel}).`);
+      db.setProfilingLevel(2);
     }
-  }).forEach(printjson);
-}
+    profilingStartTs = ISODate();
+  },
 
+  stop() {
+    if (lastProfilingLevel !== 2) {
+      print(`Setting profiling level back to ${lastProfilingLevel}.`);
+      db.setProfilingLevel(2);
+    }
+    db.system.profile.find({
+      op: 'query',
+      ts: {
+        $gt: profilingStartTs,
+        $lte: ISODate()
+      }
+    }).forEach(printjson);
+  },
+}
