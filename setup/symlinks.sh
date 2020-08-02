940c9cdab5e9c22ff1ddaba8a2951a7ede4da21e
Arik Maor <arikmaor@gmail.com>
2020-08-03 01:31:03 +0300

    Add aliases to .zshenv

--- setup/symlinks.sh
+++ setup/symlinks.sh
@@ -2,3 +2,4 @@ ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 ln -s ~/.dotfiles/.mongorc.js ~/.mongorc.js
+ln -s ~/.dotfiles/.zshenv ~/.zshenv
