a45deb17818bd2848361e92d6c3d825290f18bcf
Arik Maor <arik.maor@tensorleap.ai>
2022-04-05 14:46:06 +0300

    Link mongrc and mongoshrc

--- setup/symlinks.sh
+++ setup/symlinks.sh
@@ -2,6 +2,7 @@ ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 ln -s ~/.dotfiles/.mongorc.js ~/.mongorc.js
+ln -s ~/.dotfiles/.mongorc.js ~/.mongoshrc.js
 ln -s ~/.dotfiles/.zshenv ~/.zshenv
 ln -s ~/.dotfiles/.editorconfig ~/.editorconfig
 ln -s ~/.dotfiles/karabiner ~/.config/karabiner
