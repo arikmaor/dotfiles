68e255a591bc41f68c41657d560f607571ad6239
Arik Maor <arik.maor@tensorleap.ai>
2023-01-30 16:59:53 +0200

    Update nvim-tree settings

--- nvim/after/plugin/tree.lua
+++ nvim/after/plugin/tree.lua
@@ -1,3 +1,24 @@
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  open_on_setup = true,
+  view = {
+    mappings = {
+      list = {
+        { key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit_no_picker" },
+        { key = "O",                              action = "edit" },
+      }
+    }
+  },
+  update_focused_file = {
+    enable = true,
+  },
+  actions = {
+    open_file = {
+      quit_on_open = true,
+    }
+  }
+})
 
-vim.keymap.set('n', '<leader>v', vim.cmd.NvimTreeToggle)
+local api = require("nvim-tree.api")
+
+vim.keymap.set('n', '<leader>v', api.tree.toggle)
+vim.keymap.set('n', '<leader>b', api.tree.focus)
