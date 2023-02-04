a67ad78595ec6260635308d8bae108f6d0162a1e
Arik Maor <arik.maor@tensorleap.ai>
2023-02-04 23:01:29 +0200

    Update nvim key bindings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -7,16 +7,21 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.set_preferences({
-  set_lsp_keymaps = {omit = {'gr'}}
+  set_lsp_keymaps = { omit = { 'gr', 'gd' } }
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set('n', '<leader>rn', function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set('n', '<leader>rr', function() vim.lsp.buf.references() end, opts)
   vim.keymap.set('n', '<leader>ac', function() vim.lsp.buf.code_action() end, opts)
+  -- vim.keymap.set('n', '<leader>rr', function()
+  --   vim.lsp.buf.references()
+  -- end, opts)
 end)
+
+-- vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+vim.cmd [[autocmd BufWritePre * :EslintFixAll]]
 
 -- When you don't have mason.nvim installed
 -- You'll need to list the servers installed in your system
@@ -34,7 +39,6 @@ local servers = {
   'yamlls'
 }
 lsp.ensure_installed(servers)
-lsp.setup_servers(servers)
 
 -- (Optional) Configure lua language server for neovim
 lsp.nvim_workspace()
--- nvim/after/plugin/tree.lua
+++ nvim/after/plugin/tree.lua
@@ -1,5 +1,4 @@
 require("nvim-tree").setup({
-  open_on_setup = true,
   view = {
     mappings = {
       list = {
