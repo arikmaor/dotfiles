5bc7184f300200c017a9b44ddbe0e934e1314ed1
Arik Maor <arik.maor@tensorleap.ai>
2023-02-04 23:00:53 +0200

    Disable swap files

--- nvim/lua/custom/settings.lua
+++ nvim/lua/custom/settings.lua
@@ -2,8 +2,9 @@ vim.o.number = true
 vim.o.relativenumber = true
 
 vim.o.swap = false
+vim.o.swapfile = false
 vim.o.backup = false
-vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.o.undodir = vim.fn.stdpath('state') .. "/undodir"
 vim.o.undofile = true
 
 vim.o.scrolloff = 8
