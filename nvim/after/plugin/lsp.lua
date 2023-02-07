f442222567948ab309ca79da0dd465ae1d4e50dc
Arik Maor <arik.maor@tensorleap.ai>
2023-02-07 11:52:48 +0200

    Remove duplicated lines

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,11 +1,6 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
--- Learn the keybindings, see :help lsp-zero-keybindings
--- Learn to configure LSP servers, see :help lsp-zero-api-showcase
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
-
 lsp.set_preferences({
   set_lsp_keymaps = { omit = { 'gr', 'gd' } }
 })
