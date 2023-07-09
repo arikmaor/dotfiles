e67cfd67e3547cfbf49c5e93f0fb7b4cd568c256
Arik Maor <arikmaor@gmail.com>
2023-07-09 18:37:39 +0300

    Remove help lsp

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 return require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript", "python", "lua", "vim", "help" },
+  ensure_installed = { "javascript", "typescript", "python", "lua", "vim" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
