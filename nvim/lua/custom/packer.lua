1255b833e68052fe0d1577d8bd30b9ddb75786d8
Arik Maor <arikmaor@gmail.com>
2024-08-13 13:16:39 +0300

    Tag telescope version

--- nvim/lua/custom/packer.lua
+++ nvim/lua/custom/packer.lua
@@ -57,7 +57,7 @@ return require('packer').startup(function(use)
 
   -- fuzzy finder
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
