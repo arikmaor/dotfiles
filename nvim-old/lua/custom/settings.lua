3bef30b5f23923346c8249d1a5f7ebba2295e668
Arik Maor <arikmaor@gmail.com>
2024-08-16 20:49:20 +0300

    Move old nvim configs

--- nvim-old/lua/custom/settings.lua
+++ nvim-old/lua/custom/settings.lua
@@ -4,9 +4,9 @@ vim.o.relativenumber = true
 vim.o.swap = false
 vim.o.swapfile = false
 vim.o.backup = false
-vim.o.undodir = vim.fn.stdpath('state') .. "/undodir"
+vim.o.undodir = vim.fn.stdpath("state") .. "/undodir"
 vim.o.undofile = true
 
 vim.o.scrolloff = 8
 
-vim.o.guicursor='a:block-Cursor'
+vim.o.guicursor = "a:block-Cursor"
