099760162a93cd5de8d3a58b98b5cd94b18bd04e
Arik Maor <arikmaor@gmail.com>
2024-08-16 22:49:17 +0300

    Fix references hotkey

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      keys[#keys + 1] = { "gr", false }
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      {
+        "<leader>rr",
+        function()
+          require("telescope.builtin").lsp_references()
+        end,
+        desc = "Find References",
+      },
+    },
+  },
+}
