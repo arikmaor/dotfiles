bfbaa5dc56ff8a2fc4f3923edd5032c468accd14
Arik Maor <arikmaor@gmail.com>
2024-09-02 16:38:53 +0300

    Install lazygit and update plugins

---
+++ nvim/lua/plugins/flatten.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "willothy/flatten.nvim",
+    config = true,
+    -- opts = {  }
+    -- Ensure that it runs first to minimize delay when opening file from terminal
+    lazy = false,
+    priority = 1001,
+  },
+}
