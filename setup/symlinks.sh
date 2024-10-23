0edadac32881dfc7f39b66f6772e40a2350948fc
Arik Maor <arikmaor@gmail.com>
2024-10-23 17:32:49 +0300

    Remove editorconfig

--- setup/symlinks.sh
+++ setup/symlinks.sh
@@ -5,9 +5,7 @@ ln -s ~/dotfiles/lvim ~/.config/lvim
 ln -s ~/dotfiles/.mongorc.js ~/.mongorc.js
 ln -s ~/dotfiles/.mongorc.js ~/.mongoshrc.js
 ln -s ~/dotfiles/.zshenv ~/.zshenv
-ln -s ~/dotfiles/.editorconfig ~/.editorconfig
 ln -s ~/dotfiles/karabiner ~/.config/karabiner
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.alacritty.yml ~/.alacritty.yml
 ln -s ~/dotfiles/fonts ~/.local/share/fonts
-
