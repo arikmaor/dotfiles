2cd7c4a3da51eb6c0a43c0a62e4eaafa55f22b85
Arik Maor <arikmaor@gmail.com>
2024-10-25 01:39:24 +0300

    Switch to apt

--- setup/basic-setup.sh
+++ setup/basic-setup.sh
@@ -11,11 +11,11 @@ fi
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
-echo Running apt-get update and upgrade
-apt-get update & apt-get upgrade
+echo Running apt update and upgrade
+apt update & apt upgrade
 
 echo Installing basic apt-get stuff
-apt-get install -y build-essential procps curl wget file git tmux brave-browser alacritty zsh
+apt install -y build-essential procps curl wget file git tmux brave-browser alacritty zsh
 
 echo Setting zsh as default shell
 chsh -s $(which zsh)
