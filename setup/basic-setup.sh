9f0841b71f89dc5f1fe9e8f873fb7397ad024b27
Arik Maor <arikmaor@gmail.com>
2024-10-25 01:37:12 +0300

    Add brave brower apt repo

--- setup/basic-setup.sh
+++ setup/basic-setup.sh
@@ -5,6 +5,10 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+# brave-browser
+curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
 echo Running apt-get update and upgrade
 apt-get update & apt-get upgrade
 
