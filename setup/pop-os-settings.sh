44129fc742663ebdf10c363236c47d8de02c4506
Arik Maor <arikmaor@gmail.com>
2024-10-25 13:23:48 +0300

    Fix pop-os settings script

--- setup/pop-os-settings.sh
+++ setup/pop-os-settings.sh
@@ -2,10 +2,10 @@
 dconf write /org/gnome/shell/extensions/dash-to-dock/dash-max-icon-size 36
 
 # Center clock
-dconf write /org/gnome/shell/extensions/pop-cosmic/clock-alignment 'CENTER'
+dconf write /org/gnome/shell/extensions/pop-cosmic/clock-alignment '"CENTER"'
 
 # Show all button on app titles
-dconf write /org/gnome/desktop/wm/preferences/button-layout 'appmenu:minimize,maximize,close'
+dconf write /org/gnome/desktop/wm/preferences/button-layout '"appmenu:minimize,maximize,close"'
 
 # Auto hide dock
 dconf write /org/gnome/shell/extensions/dash-to-dock/dock-fixed false
@@ -17,7 +17,7 @@ dconf write /org/gnome/shell/extensions/dash-to-dock/multi-monitor true
 dconf write /org/gnome/mutter/workspaces-only-on-primary true
 
 # Disable power button
-dconf write /org/gnome/settings-daemon/plugins/power/power-button-action 'nothing'
+dconf write /org/gnome/settings-daemon/plugins/power/power-button-action '"nothing"'
 
 # Show battery percentage
 dconf write /org/gnome/desktop/interface/show-battery-percentage true
@@ -26,8 +26,8 @@ dconf write /org/gnome/desktop/interface/show-battery-percentage true
 dconf write /org/gnome/desktop/input-sources/per-window true
 
 # Switch input source keybinding
-dconf write /org/gnome/desktop/wm/keybindings/switch-input-source-backward "['<Primary><Shift>space']"
-dconf write /org/gnome/desktop/wm/keybindings/switch-input-source "['<Primary>space']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-input-source-backward '["<Primary><Shift>space"]'
+dconf write /org/gnome/desktop/wm/keybindings/switch-input-source '["<Primary>space"]'
 
 # Disable window menu keybinding
-dconf write /org/gnome/desktop/wm/keybindings/activate-window-menu @as []
+dconf write /org/gnome/desktop/wm/keybindings/activate-window-menu '@as []'
