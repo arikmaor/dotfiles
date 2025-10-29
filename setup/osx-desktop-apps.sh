a3914c1746d8666a85a9288855bd7218e94ab473
Arik Maor <arik.maor@applitools.com>
2025-10-29 11:15:52 +0200

    Update osx-desktop-apps.sh

--- setup/osx-desktop-apps.sh
+++ setup/osx-desktop-apps.sh
@@ -8,4 +8,51 @@ brew install --cask \
   brave-browser \
   slack \
   visual-studio-code \
-  rider
+  rider \
+  google-cloud-sdk \
+  powershell \
+  docker-desktop \
+  vlc \
+  zoom \
+  insta360-studio \
+  qbittorrent \
+  gcloud-cli \
+  windows-app
+
+brew install \
+  azure-cli \
+  btop \
+  fd \
+  ffmpeg \
+  fzf \
+  gcc \
+  gh \
+  go \
+  helm \
+  htop \
+  jq \
+  k3d \
+  k9s \
+  kapp \
+  krew \
+  kubectx \
+  kubernetes-cli \
+  lazygit \
+  neovim \
+  ranger \
+  opencv \
+  readline \
+  ripgrep \
+  stern \
+  stow \
+  tcld \
+  temporal \
+  tesseract \
+  tfenv \
+  tig \
+  tmux \
+  tree \
+  watch \
+  wget \
+  yt-dlp \
+  z
