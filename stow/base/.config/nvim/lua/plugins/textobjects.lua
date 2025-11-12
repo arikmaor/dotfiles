8981815d360046b0b317a316dc17527be4bc108c
Arik Maor <arikmaor@gmail.com>
2025-11-12 15:44:50 +0200

    Add ReplaceWithRegister plugin

--- stow/base/.config/nvim/lua/plugins/textobjects.lua
+++ stow/base/.config/nvim/lua/plugins/textobjects.lua
@@ -1,6 +1,11 @@
 return {
   "vim-scripts/argtextobj.vim",
-  "vim-scripts/ReplaceWithRegister",
+  {
+    "vim-scripts/ReplaceWithRegister",
+    keys = {
+      { "<leader>gr", "<Plug>ReplaceWithRegisterOperator", desc = "ReplaceWithRegisterOperator" },
+    },
+  },
   "michaeljsmith/vim-indent-object",
   "tpope/vim-surround",
   "tpope/vim-repeat",
